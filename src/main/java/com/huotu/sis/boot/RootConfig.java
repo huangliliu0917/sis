06ab1837d27d9ab8eb97aab4b92f7ba01b4ade53
shiliting <861819089@qq.com>
2016-03-22 16:58:47 +0800

    common service

--- src/main/java/com/huotu/sis/boot/RootConfig.java
+++ src/main/java/com/huotu/sis/boot/RootConfig.java
@@ -3,6 +3,7 @@ package com.huotu.sis.boot;
 import com.huotu.huobanplus.common.CommonServiceSpringConfig;
 import com.huotu.huobanplus.sdk.common.CommonClientSpringConfig;
 import com.huotu.huobanplus.sdk.mall.MallSDKConfig;
+import com.huotu.huobanplus.smartui.SmartUIServiceSpringConfig;
 import org.luffy.lib.libspring.data.ClassicsRepositoryFactoryBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan({"com.huotu.sis.service"})
 @EnableJpaRepositories(value = "com.huotu.sis.repository", repositoryFactoryBeanClass = ClassicsRepositoryFactoryBean.class)
 @ImportResource(value = {"classpath:spring-jpa.xml"})
-@Import(value = {CommonClientSpringConfig.class, MallSDKConfig.class, CommonServiceSpringConfig.class})
+@Import(value = {CommonClientSpringConfig.class, MallSDKConfig.class, CommonServiceSpringConfig.class, SmartUIServiceSpringConfig.class})
 public class RootConfig {
 
 
