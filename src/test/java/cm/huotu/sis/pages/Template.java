65166ef5bedf864d298c33cbbe192b22d222f687
jin346952524 <346952524@qq.com>
2016-03-28 16:19:12 +0800

    模板测试

--- src/test/java/cm/huotu/sis/pages/Template.java
+++ src/test/java/cm/huotu/sis/pages/Template.java
@@ -1,9 +1,13 @@
 package cm.huotu.sis.pages;
 
 import com.huotu.huobanplus.smartui.entity.TemplatePage;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -29,5 +33,11 @@ public class Template extends AbstractPage {
         List<WebElement> pList = list.get(0).findElements(By.cssSelector("p"));
         assertThat(pList.get(0).getText().trim()).isEqualTo(templatePage.get(0).getTitle())
                 .as("模板标题的比对");
+        WebElement button = pList.get(2).findElement(By.cssSelector("button"));
+        button.click();
+        //点击了按钮之后，这个button的text预期变成了“已选择该模板”
+        Boolean isChange = new WebDriverWait(webDriver,30)
+                .until(ExpectedConditions.textToBePresentInElement(button,"已选择该模板"));
+        assertThat(isChange);
     }
 }
