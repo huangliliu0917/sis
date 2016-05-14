6f35e08242bf71e9f7cef82db5247956df3a3feb
lgh <106517651@qq.com>
2016-05-14 10:29:45 +0800

    我的团队修改

--- src/main/java/com/huotu/sis/repository/IndirectPushFlowRepository.java
+++ src/main/java/com/huotu/sis/repository/IndirectPushFlowRepository.java
@@ -4,6 +4,7 @@ package com.huotu.sis.repository;
 import com.huotu.huobanplus.common.entity.User;
 import com.huotu.sis.entity.IndirectPushFlow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,4 +13,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IndirectPushFlowRepository extends JpaRepository<IndirectPushFlow, Long> {
     Long countByTotalGenerationTwoUser(User user);
+
+    /**
+     * 统计今日总数
+     * @param user 用户
+     * @param time 今日凌晨时间
+     * @return
+     */
+    @Query("select count(flow) from IndirectPushFlow flow where flow.totalGenerationTwoUser=?1 and flow.time>=?2")
+    Long countTodayByTotalGenerationTwoUser(User user, Long time);
 }
